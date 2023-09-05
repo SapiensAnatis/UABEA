547a0554434fd0dc0399be7134317fdc88119d7d
Jay Malhotra <jay0@me.com>
2023-09-05 21:01:37 +0100

    Add serialized dict plugin

---
+++ SerializedDictionaryPlugin/Options/ExportDictionaryOption.cs
@@ -0,0 +1,64 @@
+﻿using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UABEAvalonia;
+using UABEAvalonia.Plugins;
+
+namespace SerializableDictionaryPlugin.Options;
+
+public class ExportDictionaryOption : DictionaryOption
+{
+    protected override string OptionName => "Export serialized dictionary as JSON";
+
+    public override async Task<bool> ExecutePlugin(
+        Window win,
+        AssetWorkspace workspace,
+        List<AssetContainer> selection
+    )
+    {
+        AssetContainer cont = selection[0];
+        AssetTypeValueField? baseField = workspace.GetBaseField(cont);
+
+        ArgumentNullException.ThrowIfNull(baseField);
+
+        var dict = baseField.Children.First(x => x.FieldName == "dict");
+        IEnumerable<object> keys = dict.Children
+            .First(x => x.FieldName == "entriesKey")
+            .Children.First(x => x.FieldName == "Array")
+            .Children.Select(x => x.AsObject)
+            .Where(x => IsNonEmptyKey(x));
+
+        IEnumerable<Dictionary<string, object>> values = dict.Children
+            .First(x => x.FieldName == "entriesValue")
+            .Children.First(x => x.FieldName == "Array")
+            .Children.Select(x => x.Children.ToDictionary(c => c.FieldName, c => c.AsObject));
+
+        IStorageFile? saveFile = await win.StorageProvider.SaveFilePickerAsync(
+            new FilePickerSaveOptions() { Title = "Save JSON file", FileTypeChoices = JsonFilter }
+        );
+        string? path = FileDialogUtils.GetSaveFileDialogFile(saveFile);
+
+        if (path is null)
+            return false;
+
+        SerializableDictionaryHelper.WriteToFile(path, keys, values);
+
+        return true;
+    }
+
+    private static bool IsNonEmptyKey(object key)
+    {
+        return key switch
+        {
+            int intValue => intValue is not 0,
+            string stringValue => !string.IsNullOrEmpty(stringValue),
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
---
+++ SerializedDictionaryPlugin/SerializableDictionaryHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace SerializableDictionaryPlugin;
+
+public static class SerializableDictionaryHelper
+{
+    public static SerializableDictionary<dynamic, object> CreateFromFile(string filepath)
+    {
+        using FileStream fs = File.OpenRead(filepath);
+
+        Dictionary<object, object>? baseDictionary = JsonSerializer.Deserialize<
+            Dictionary<object, object>
+        >(fs);
+
+        if (baseDictionary is null || !baseDictionary.Any())
+            throw new ArgumentException("Null or empty dictionary");
+
+        object firstKey = baseDictionary.Keys.First();
+        return firstKey switch
+        {
+            string s
+                => new SerializableDictionary<object, object>(
+                    baseDictionary,
+                    DeterministicStringEqualityComparer.Instance
+                ),
+            _ => new SerializableDictionary<object, object>(baseDictionary),
+        };
+    }
+
+    public static void WriteToFile(
+        string filepath,
+        IEnumerable<object> keys,
+        IEnumerable<object> values
+    )
+    {
+        Dictionary<object, object> dict = keys.Zip(values)
+            .ToDictionary(x => x.First, x => x.Second);
+
+        using FileStream fs = File.OpenWrite(filepath);
+        JsonSerializer.Serialize(fs, dict, new JsonSerializerOptions() { WriteIndented = true });
+    }
+}
